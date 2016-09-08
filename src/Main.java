f28453baf22db24187c2eb4cd7617d05e0f3f893
GitToTheChoppaaa <c.hammond@msn.com>
2016-09-08 15:33:49 +0100

    added try-catch to main.java , to check if the DB was correctly loaded

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,19 @@ public class Main {
 	public static void main(String[] args) {
 		
 		DaterData daterData = new DaterData();
-		daterData.loadDataFromFile("/Users/chrishammond/Documents/Eclipse/Workspace/CapitaJava1/src/DaterData3.csv" );
+		daterData.loadDataFromFile("/Users/chrishammond/Documents/Eclipse/Workspace/CapitaJava1/src/DaterData34.csv" );
 		
 		System.out.println("Number of records in database: " + daterData.getNumberOfRecords());
+		try{
 		System.out.println(daterData.getRecordNumber(randomNo()));
-		
+		}
+		catch(Exception e){
+			System.out.println("Something went wrong... " + e);
+			
+			if ((daterData.getNumberOfRecords()) == 0){
+				System.out.println("DB empty or not found");
+			}
+		}
 	}
 	
 	public static int randomNo(){ 
